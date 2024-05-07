05f14c61e584b0a5a04c820ad6954e0c29458ddc
AK1612 <159476917+AK1612@users.noreply.github.com>
2024-05-07 21:27:45 +0530

    msl.py

--- msl.py
+++ msl.py
@@ -19,16 +19,17 @@ query="""create table channel(channel_id varchar(255) primary key,
 cursor.execute(query)
 #table playlist
 query="""create table playlist(playlist_id varchar(255) primary key,
-       channel_id varchar(255), playlist_name varchar(255), foreign key(channel_id) references channel(channel_id))"""
+       channel_id varchar(255), playlist_name varchar(255))"""
 cursor.execute(query)
 #table video
 query="""create table video(video_id varchar(255) primary key,
        playlist_id varchar(255), video_name varchar(255), video_description text,
-       view_count int,like_count int,favorite_count int,comment_count int,duration int, thumbnail varchar(255),published_date datetime, year int, foreign key(playlist_id) references playlist(playlist_id) )"""
+       view_count int,like_count int,favorite_count int,comment_count int,duration int,
+       thumbnail varchar(255),published_date datetime, year int)"""
 cursor.execute(query)
 #table comment
 query="""create table comment(comment_id varchar(255) primary key, video_id varchar(255) , comment text, comment_author varchar(255),
-       comment_pub_date datetime, foreign key(video_id) references video(video_id))"""
+       comment_pub_date datetime)"""
 cursor.execute(query)
 
 
