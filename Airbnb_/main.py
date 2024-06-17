bd12a59bcd6577478fd8dac0e491a054394fd93b
AK1612 <159476917+AK1612@users.noreply.github.com>
2024-06-17 23:40:25 +0530

    Add files via upload

---
+++ Airbnb_/main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="Airbnb Data Visualisation")
+
+st.title("Airbnb User Friendly Data Visualisation")
+st.sidebar.success("select page above.")
+
+
