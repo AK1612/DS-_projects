f1086100c95ade2b16f3fbf8d2ac86ea818c23a9
AK1612 <159476917+AK1612@users.noreply.github.com>
2025-04-28 21:23:07 +0530

    Delete Airbnb_/main.py

--- Airbnb_/main.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-
-st.set_page_config(page_title="Airbnb Data Visualisation")
-
-st.title("Airbnb User Friendly Data Visualisation")
-st.sidebar.success("select page above.")
-
-
